62d00422013e7e72a26ec5ef82a4334148000fce
usopp-HP <you@example.com>
2019-12-12 11:55:46 +1100

    added check for plugged in or not

--- shutdown.py
+++ shutdown.py
@@ -9,7 +9,7 @@ while x == 0:
     battery = psutil.sensors_battery()
     plugged = battery.power_plugged
     percent = battery.percent
-    if percent < 98:
+    if percent < 50 and plugged==False:
         print("Under threshold, shuuting down")
         print(os.system("shutdown now"))
     if plugged==False: plugged="Not Plugged In"
