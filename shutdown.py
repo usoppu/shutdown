8e68eb2aeb0b6586c9c3e8ac7a61c67cea9dcdfd
usopp-HP <you@example.com>
2019-12-12 11:48:35 +1100

    init

---
+++ shutdown.py
@@ -0,0 +1,19 @@
+#requirement pip3 install psutil
+
+import psutil
+import time
+import os
+ 
+x=0
+while x == 0:
+    battery = psutil.sensors_battery()
+    plugged = battery.power_plugged
+    percent = battery.percent
+    if percent < 98:
+        print("Under threshold, shuuting down")
+        print(os.system("shutdown now"))
+    if plugged==False: plugged="Not Plugged In"
+    else: plugged="Plugged In"
+    print(str(percent)+'% | '+plugged)
+    time.sleep(30)
+    
